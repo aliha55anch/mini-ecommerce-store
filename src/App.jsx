939619e892b0382837039e69ec86c8ffe9645228
aliha55anch <chali37125@gmail.com>
2026-07-29 21:05:20 +0500

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [cartOpen, setCartOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-50">no
+    <div className="min-h-screen bg-gray-50">
     
       <Navbar onCartClick={() => setCartOpen(true)} />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
